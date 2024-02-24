5dc284461ff5c34f7d04d676fdc91198d133ab73
J-Oskam <151467881+J-Oskam@users.noreply.github.com>
2024-02-24 10:23:40 -0500

    changed nav bar colours.

--- COMP2139-Labs/obj/Debug/net8.0/COMP2139-Labs.AssemblyInfo.cs
+++ COMP2139-Labs/obj/Debug/net8.0/COMP2139-Labs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COMP2139-Labs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+812662da3ec697f5ead6dc89c311e2e07bfbc195")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b04fb3241ab4b02a95c99aabb7606cf36ab503")]
 [assembly: System.Reflection.AssemblyProductAttribute("COMP2139-Labs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COMP2139-Labs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
